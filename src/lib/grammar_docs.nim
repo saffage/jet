43886885dbddcbc60bf8a511bb0d10efd2f7363d
Saffage <04coder@gmail.com>
2023-12-26 14:35:13 +0200

    lib: don't build grammar in macro when not defined 'jetGrammarDocs'

--- src/lib/grammar_docs.nim
+++ src/lib/grammar_docs.nim
@@ -32,63 +32,66 @@ proc getGrammar*(): string
     ## **Returns:** all currently builded grammar
     return grammarBuffer
 
-macro grammarDocs*(node: typed) =
-    ## Pragma to scan comment of the procedure and extract a
-    ## grammar from it.
-    ## To mark a block of grammar write `@grammar` in the beginnig
-    ## of a grammar block and `@end` in the end.
-    ##
-    ## To build grammar just call `getGrammar()` proc.
-    case node.kind
-    of RoutineNodes:
-        if node.body.kind == nnkEmpty:
-            warning("Can't get a comment (declarations are not supported)", node)
-            return node
-        elif node.body[0].kind != nnkCommentStmt:
-            warning("No comments provided for building the grammar", node)
-        else:
-            let comment        = node.body[0]
-            var inGrammarBlock = false
-            var wasWritten     = false
-            var openLineOffset = 0
+when not defined(jetGrammarDocs):
+    macro grammarDocs*(node: typed) = node
+else:
+    macro grammarDocs*(node: typed) =
+        ## Pragma to scan comment of the procedure and extract a
+        ## grammar from it.
+        ## To mark a block of grammar write `@grammar` in the beginnig
+        ## of a grammar block and `@end` in the end.
+        ##
+        ## To build grammar just call `getGrammar()` proc.
+        case node.kind
+        of RoutineNodes:
+            if node.body.kind == nnkEmpty:
+                warning("Can't get a comment (declarations are not supported)", node)
+                return node
+            elif node.body[0].kind != nnkCommentStmt:
+                warning("No comments provided for building the grammar", node)
+            else:
+                let comment        = node.body[0]
+                var inGrammarBlock = false
+                var wasWritten     = false
+                var openLineOffset = 0
 
-            for lineOffset, line in splitLines($comment).pairs():
-                var annotation = getAnnotation(line)
+                for lineOffset, line in splitLines($comment).pairs():
+                    var annotation = getAnnotation(line)
 
-                if annotation.cmpIgnoreStyle("grammar") == 0:
-                    inGrammarBlock = true
-                    openLineOffset = lineOffset
-                    continue
-                elif annotation.cmpIgnoreStyle("end") == 0:
-                    if not inGrammarBlock:
-                        warning("Missing opening '@grammar'", warningWithLineInfo("end", comment.lineInfoObj, lineOffset))
+                    if annotation.cmpIgnoreStyle("grammar") == 0:
+                        inGrammarBlock = true
+                        openLineOffset = lineOffset
                         continue
-                    inGrammarBlock = false
-                    continue
+                    elif annotation.cmpIgnoreStyle("end") == 0:
+                        if not inGrammarBlock:
+                            warning("Missing opening '@grammar'", warningWithLineInfo("end", comment.lineInfoObj, lineOffset))
+                            continue
+                        inGrammarBlock = false
+                        continue
+
+                    if inGrammarBlock:
+                        wasWritten = true
+                        grammarBuffer.add(line & '\n')
 
                 if inGrammarBlock:
-                    wasWritten = true
-                    grammarBuffer.add(line & '\n')
+                    warning("Missing closing '@end'", warningWithLineInfo("grammar", comment.lineInfoObj, openLineOffset))
 
-            if inGrammarBlock:
-                warning("Missing closing '@end'", warningWithLineInfo("grammar", comment.lineInfoObj, openLineOffset))
-
-            if not wasWritten:
-                warning("Missing or empty grammar block in the comment", comment)
-            else:
-                grammarBuffer.add('\n')
-        result = node
-    of nnkStmtList:
-        if node.len() > 1 or node[0].kind != nnkCommentStmt:
-            let errNode =
-                if node.len() <= 1: node
-                else: node[1]
-            error("The only comment statement accepted for 'StmtList' for building grammar from it", errNode)
-        grammarBuffer.add(node[0].strVal & "\n\n")
-        result = newEmptyNode()
-    else:
-        error("Invalid node to build grammar", node)
-        result = node
+                if not wasWritten:
+                    warning("Missing or empty grammar block in the comment", comment)
+                else:
+                    grammarBuffer.add('\n')
+            result = node
+        of nnkStmtList:
+            if node.len() > 1 or node[0].kind != nnkCommentStmt:
+                let errNode =
+                    if node.len() <= 1: node
+                    else: node[1]
+                error("The only comment statement accepted for 'StmtList' for building grammar from it", errNode)
+            grammarBuffer.add(node[0].strVal & "\n\n")
+            result = newEmptyNode()
+        else:
+            error("Invalid node to build grammar", node)
+            result = node
 
 
 when isMainModule:
