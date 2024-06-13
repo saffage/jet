a72777dc983c00fdb1e1821d5e429332c97bfc34
Saffage <04coder@gmail.com>
2024-06-13 19:16:41 +0300

    chore: make errors of `token.IsValidIdent` predefined

--- token/token.go
+++ token/token.go
@@ -1,11 +1,20 @@
 package token
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
 	"unicode/utf8"
 
 	"github.com/galsondor/go-ascii"
+)
+
+var (
+	ErrorFirstIsNotLetter = errors.New("the first character must be an ASCII letter")
+	ErrorContainSpace     = errors.New("identifier cannot contain spaces")
+	ErrorContainPunct     = errors.New("identifier cannot contain a punctuation character")
+	ErrorUnsupportedUTF8  = errors.New("UTF-8 is not supported")
+	ErrorIllegalCharacter = errors.New("illegal character in the identifier")
 )
 
 type Token struct {
@@ -86,27 +95,25 @@ func IsIdentifierChar(char byte) bool {
 
 func IsValidIdent(s string) (int, error) {
 	if !ascii.IsLetter(s[0]) {
-		return 0, fmt.Errorf("the first character must be an ASCII letter")
+		return 0, ErrorFirstIsNotLetter
 	}
 
 	for i := 1; i < len(s); i++ {
 		switch {
-		case ascii.IsLetter(s[i]):
-		case ascii.IsDigit(s[i]):
-
-		case s[i] == '_':
+		case ascii.IsLetter(s[i]), ascii.IsDigit(s[i]), s[i] == '_':
+			// OK
 
 		case ascii.IsSpace(s[i]):
-			return i, fmt.Errorf("identifier cannot contain spaces")
+			return i, ErrorContainSpace
 
 		case ascii.IsPunct(s[i]):
-			return i, fmt.Errorf("identifier cannot contain a punctuation character")
+			return i, ErrorContainPunct
 
 		case utf8.RuneStart(s[i]):
-			return i, fmt.Errorf("UTF-8 is not supported")
+			return i, ErrorUnsupportedUTF8
 
 		default:
-			return i, fmt.Errorf("invalid character for identifier")
+			return i, ErrorIllegalCharacter
 		}
 	}
 
