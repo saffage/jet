dc3dde0edd007efe30ee4fec49d5225f979a9577
Saffage <04coder@gmail.com>
2024-08-18 22:28:58 +0300

    More descriptive file name

--- token/json_encode_decode.go
+++ token/json_encode_decode.go
@@ -5,6 +5,10 @@ import (
 	"fmt"
 )
 
+func (kind Kind) MarshalJSON() ([]byte, error) {
+	return json.Marshal(kind.String())
+}
+
 func (kind *Kind) UnmarshalJSON(data []byte) error {
 	name := string(data)
 	k := KindFromString(name)
@@ -15,8 +19,4 @@ func (kind *Kind) UnmarshalJSON(data []byte) error {
 
 	*kind = k
 	return nil
-}
-
-func (kind Kind) MarshalJSON() ([]byte, error) {
-	return json.Marshal(kind.String())
 }
